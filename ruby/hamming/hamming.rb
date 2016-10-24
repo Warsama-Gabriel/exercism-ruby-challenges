96d8747a2b759aee9dd9bfd11accdbee90bf2e65
chrome-tempest <srobleh@gmail.com>
2016-10-23 17:37:30 -0700

    added second iteration for solution to the hamming problem

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,25 +1,20 @@
+module BookKeeping
+  VERSION = 3
+end
+
+
 class Hamming
 
   require 'pry'
 
-  def self.compute(a,b)
-    strand_1 = a.split('')
-    strand_2 = b.split('')
-    distance = 0
-    i = 0
+  def self.compute(strand_1, strand_2)
+
     if !strand_1.length.eql?(strand_2.length)
       raise ArgumentError, 'Invalid parameters, strands must be of equal lengths'
-    else
-      until i == strand_1.length do
-        position_strand1 = strand_1[i]
-        position_strand2 = strand_2[i]
-
-        if !position_strand1.eql?(position_strand2)
-          distance += 1
-        end
-        i += 1
-      end
     end
-    distance
+
+    (0...strand_1.length).count do |i|
+      strand_1[i] != strand_2[i]
+    end
   end
 end
