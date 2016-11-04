e9ae6e61145f9695df24e678af88490671d3f0a1
chrome-tempest <srobleh@gmail.com>
2016-11-04 16:54:34 -0700

    Iteration to improve solution

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,17 @@
+
+class Complement
+
+  def self.of_dna(dna)
+
+    dna_strand = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+    if dna.chars.all? {|letter| dna_strand.include?(letter)}
+      dna.chars.map {|letter| dna_strand[letter]}
+    else
+      ""
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
