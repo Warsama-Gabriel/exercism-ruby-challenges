6ecb1952bf00027dda95dde9c85461ecf156b821
Warsama Gabriel <srobleh@gmail.com>
2017-06-10 18:48:53 -0700

    added solution for pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,2 +1,11 @@
+module BookKeeping
+  VERSION = 4
+end
+
 class Pangram
+   def self.pangram?(phrase)
+    words = phrase.downcase
+    letters = "a".."z"
+    letters.all? {|letter| words.count(letter) > 0 }
+  end
 end
